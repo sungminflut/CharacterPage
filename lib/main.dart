70141b584f35a8a25a44095f0a4082df3b25273a
sungminflut <117615219+sungminflut@users.noreply.github.com>
2022-11-14 17:30:24 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'BBANTO',
+      title: 'BBANTO', // named argument
       home: Grade(),
     );
   }
